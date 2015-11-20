20803826978ec0d0076141f93ea2a781bafe3be1
Brian <bgrober@gmail.com>
2015-11-20 14:29:28 -0800

    added some comments to the back end in order to make it easier to understand

--- server/server.js
+++ server/server.js
@@ -9,20 +9,23 @@ var fileController = require('./utils/fileController');
 var mkDir = require('./utils/mkDir');
 var zipFunction = require('./utils/zipFunction');
 
+//configure express
 var app = express();
 var server = http.createServer(app);
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './../')));
 
+//have the index html send on root route
 app.get('/', function(req,res) {
   res.sendFile('/index.html');
 });
 
+//post route for when the user is done setting up their component layout, kicks off middleware chain to create directory, write files to created directory, then zip file.
 app.post('/submit', mkDir, fileController, zipFunction, function(req,res) {
   res.send('ok');
 });
 
-
+//on submit route response being sent successfully, the client will set location to /download to initiate the download of the zip
 app.get('/download', function(req, res) {
   res.download(__dirname + "/../archive_name.zip");
 });
--- server/utils/createFiles.js
+++ server/utils/createFiles.js
@@ -3,8 +3,11 @@ var fs = Promise.promisifyAll(require('fs'));
 var ejs = require('ejs');
 
 function createFiles(obj, projectName, file) {
+  //writes a file to the directory created in mkDir, using the React template ejs.
   fs.writeFileSync(`./${projectName}/${obj.name}.js`, file);
+  //escape out of the recursive call
   if(!obj.children) return;
+  //else create files for all of the child components
   return obj.children.map(function (elem) {
     file = ejs.render(fs.readFileSync(__dirname + '/templates/reactTemplate.ejs', 'utf-8'), {component: elem});
     return createFiles(elem, projectName, file);
