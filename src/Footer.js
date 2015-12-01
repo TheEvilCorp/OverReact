a471ff874a58b2a28b8a2fd6df87a18448e66252
Susan Tashiro <susantashiro@gmail.com>
2015-12-01 10:45:36 -0800

    Front end started - working on application section

---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+var React = require('react');
+var $ = require('jquery');
+
+
+var Footer = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+  render: function () {
+    return (
+      <div>
+        Footer
+        
+      </div>
+    )
+  }
+});
+
+module.exports = Footer;
+
+
---
+++ src/Getstarted.js
@@ -0,0 +1,21 @@
+var React = require('react');
+var $ = require('jquery');
+
+
+var Getstarted = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+  render: function () {
+    return (
+      <div>
+        Getstarted
+        
+      </div>
+    )
+  }
+});
+
+module.exports = Getstarted;
+
+
