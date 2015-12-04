ae6adf8d17cfc2e5617aae1dfc414950ecc601b0
Susan Tashiro <susantashiro@gmail.com>
2015-12-04 14:26:39 -0800

    added project name input field

--- src/Options.js
+++ src/Options.js
@@ -10,11 +10,10 @@ var Options = React.createClass({
   render: function () {
     return (
       <div id='options-section'>
-        <h4>Options</h4>
+        <h3>Options</h3>
         <div className='form-group'>   
-          
           <Input type='text' label='Project Name'></Input>
-
+          <hr></hr>
           <button id='saveButton'>Save Template</button>
           <button id='loadButton'>Load Template</button>
         </div>
