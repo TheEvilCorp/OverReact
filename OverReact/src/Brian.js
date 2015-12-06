34613d5172e490d44be46cebfac2c967da778508
Brian <bgrober@gmail.com>
2015-12-05 17:28:51 -0800

    adding es6 begins

---
+++ OverReact/src/Brian.js
@@ -0,0 +1,17 @@
+var React = require('react');
+
+
+var Brian = React.createClass({
+  render: function () {
+    return (
+      <div id='Brian'>
+        Brian
+        
+      </div>
+    )
+  }
+});
+
+module.exports = Brian;
+
+
