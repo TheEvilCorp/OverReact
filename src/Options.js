1c700c06e5b5467f07de7ae4f4fba1815f78a20a
Susan Tashiro <susantashiro@gmail.com>
2015-12-07 10:33:46 -0800

    Modularized options component functions

--- src/Options.js
+++ src/Options.js
@@ -16,29 +16,7 @@ var Options = React.createClass({
       grunt:false
     };
   },
-
-  // disableRadioButtons: function(){
-  //   this.setState({ express: false });
-  //   this.setState({ hapi: false });
-  //   this.setState({ gulp: false });
-  //   this.setState({ grunt: false });
-  // },
-  // enableRadioButtons: function() {
   
-    // this.setState({ basic: false });
-  
-    // //if the basic option is not chosen, at least one server and one task runner will be chosen
-    // if(!$('#express').prop('checked') && !$('#hapi').prop('checked')) {
-    //   $('#express').prop('checked', true);
-    //   this.setState({ express: true });
-    //   this.setState({ hapi: false });
-    // } 
-    // if(!$('#gulp').prop('checked') && !$('#grunt').prop('checked')) {
-    //   $('#gulp').prop('checked', true);
-    //   this.setState({ gulp: true });
-    //   this.setState({ grunt: false });
-    // }
-  // },
   handleBasicBtnChange: function() {
     
     if(!this.state.basic) {
@@ -57,36 +35,9 @@ var Options = React.createClass({
     handleRadioButton(this)
     enableRadioBtns(this)
   },
+  
   post: function() {
     postFunction(this.props.id, this.props.hash);
-  },
-  handleRadioBtnChange: function(e) {
-    // handleRadioButton()
-    // enableRadioBtns();
-
-    // var id = e.target.id;
-    // this.setState({ express: $('#express').prop('checked')});
-    // this.setState({ hapi: $('#hapi').prop('checked')});
-    // this.setState({ gulp: $('#gulp').prop('checked')});
-    // this.setState({ grunt: $('#grunt').prop('checked')});
-
-    // if($('#express').prop('checked')) {
-    //   this.setState({ express: true });
-    //   this.setState({ hapi: false });
-    // }
-    // if($('#hapi').prop('checked')) {
-    //   this.setState({ hapi: true });
-    //   this.setState({ express: false });
-    // }
-    // if($('#gulp').prop('checked')) {
-    //   this.setState({ gulp: true });
-    //   this.setState({ grunt: false });
-    // }
-    // if($('#grunt').prop('checked')) {
-    //   this.setState({ grunt: true });
-    //   this.setState({ gulp: false });
-    // }
-    // this.enableRadioButtons();
   },
 
   render: function () {
