de044f4f191c976652622479c8f15a0f2f7ac10e
Susan Tashiro <susantashiro@gmail.com>
2015-12-07 18:35:34 -0800

    Added feedback and github link to footer

--- src/Footer.js
+++ src/Footer.js
@@ -16,12 +16,13 @@ var Footer = React.createClass({
         <div>
           <ul>
             <li><a href="mailto:OverReact.EvilCorp@gmail.com">Feedback</a></li>
-            <li><a href="https://github.com/TheEvilCorp/OverReact">Github</a></li>
+            <li><a target="_blank" href="https://github.com/TheEvilCorp/OverReact/tree/version1">Github</a></li>
           </ul> 
           <div>
             <p>Brought to you by the good folks at Evil Corp</p>
-            <a href='https://shop.stjude.org/GiftCatalog/donation.do?cID=13805&pID=24591&sc_icid=lz-tg-btn-donate-now'>Donate Here</a>
+            <a target="_blank" href='https://shop.stjude.org/GiftCatalog/donation.do?cID=13805&pID=24591&sc_icid=lz-tg-btn-donate-now'>Donate Here</a>
           </div>
+        </div>
       </div>
     )
   }
