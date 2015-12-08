97ff3f0163b5f65ae34afc321fdfef63b16ad71e
Brian <bgrober@gmail.com>
2015-12-07 16:19:46 -0800

    refactored radio bits

--- src/Home.js
+++ src/Home.js
@@ -7,14 +7,20 @@ var Button = require('react-bootstrap').Button;
 
 var Home = React.createClass({
   setFocus: function() {
+    $('html, body').animate({
+        scrollTop: $('#application-section').offset().top
+    }, 500);
     $('#overReact-container').find('input').focus();
+    return false;
+});
+
   },
   render: function () {
     return (
       <div id='home-section'>
         <h1>The React File Generator</h1>
         <p>Wireframe React components and download starter files with one click</p>
-        <p><Button onClick={this.setFocus} href='#application-section' bsStyle="primary" bsSize="large" id='getStarted-btn'>Get Started</Button></p>
+        <p><Button onClick={this.setFocus} bsStyle="primary" bsSize="large" id='getStarted-btn'>Get Started</Button></p>
         <Instructions />
       </div>
     )
