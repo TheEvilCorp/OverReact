fa3acc37e01a682c6de051bfe6c80aaf01d2c3c6
Rico Moorer <rico@alumni.upenn.edu>
2015-12-11 10:46:46 -0800

    working on css

--- src/Footer.js
+++ src/Footer.js
@@ -8,20 +8,20 @@ var Footer = React.createClass({
   },
   render: function () {
     var styleObj = {
-      textAlign: 'center'
+      textAlign: 'center',
     };
 
     return (
       <div style={styleObj} id='footer-section'>
-        <div>
-          <ul>
-            <li><a href="mailto:OverReact.EvilCorp@gmail.com">Feedback</a></li>
-            <li><a target="_blank" href="https://github.com/TheEvilCorp/OverReact/tree/version1">Github</a></li>
-          </ul> 
-          <div>
+        <div className='footerFlexContainer'>
+          <span className='footerFlexItem'>
+            <a id='feedback' href="mailto:OverReact.EvilCorp@gmail.com">Feedback</a>
+            <a target="_blank" href="https://github.com/TheEvilCorp/OverReact/tree/version1">Github</a>
+          </span>
+          <div className='footerFlexItem'>
             <p>Brought to you by the good folks at Evil Corp</p>
-            <a target="_blank" href='https://shop.stjude.org/GiftCatalog/donation.do?cID=13805&pID=24591&sc_icid=lz-tg-btn-donate-now'>Donate Here</a>
           </div>
+          <a className='footerFlexItem' target="_blank" href='https://shop.stjude.org/GiftCatalog/donation.do?cID=13805&pID=24591&sc_icid=lz-tg-btn-donate-now'>Donate Here</a>
         </div>
       </div>
     )
