0a6a0b27c93bc97fa022036e2c7e6fa5961ebc65
Brian <bgrober@gmail.com>
2015-12-30 10:37:59 -0800

    eb config

---
+++ .ebextensions/50npm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+. /opt/elasticbeanstalk/env.vars
+function error_exit
+{
+  eventHelper.py --msg "$1" --severity ERROR
+  exit $2
+}
+
+#install not-installed yet app node_modules
+if [ ! -d "/var/node_modules" ]; then
+  mkdir /var/node_modules ;
+fi
+if [ -d /tmp/deployment/application ]; then
+  ln -s /var/node_modules /tmp/deployment/application/
+fi
+
+OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm install 2>&1) || error_exit "Failed to run npm install.  $OUT" $?
+echo $OUT
