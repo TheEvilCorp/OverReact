41793f8231a9848fbd0ca9308b55087f3f200a26
Brian <bgrober@gmail.com>
2015-12-31 10:27:22 -0800

    change node -v to 4.2.1

--- 40install_node.sh
+++ 40install_node.sh
@@ -5,4 +5,4 @@ set -xe
 /opt/elasticbeanstalk/containerfiles/ebnode.py --action node-install
 
 # Update npm
-cd /opt/elasticbeanstalk/node-install/node-v4.2.3-linux-x64/bin/ && /opt/elasticbeanstalk/node-install/node-v4.2.3-linux-x64/bin/npm update npm -g
+cd /opt/elasticbeanstalk/node-install/node-v4.2.1-linux-x64/bin/ && /opt/elasticbeanstalk/node-install/node-v4.2.1-linux-x64/bin/npm update npm -g
