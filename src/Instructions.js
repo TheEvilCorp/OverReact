d2c169a865cb4d989c32785a46e136c3b427b784
Susan Tashiro <susantashiro@gmail.com>
2016-01-02 18:03:02 -0800

    Updated gif & fixed modal overflow

--- src/Instructions.js
+++ src/Instructions.js
@@ -5,7 +5,7 @@ export default class Instructions extends Component {
   render() {
     return (
       <div style={styles.Home.Instructions.instructionsSection}>
-        <img src="/images/testOptimized.gif" alt="HTML5 Icon" width="650px"/>
+        <img src="/images/Demo3_optimized.gif" alt="HTML5 Icon" width="650px"/>
       </div>
     )
   }
