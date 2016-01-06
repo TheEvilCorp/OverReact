214ba3ac7a226e9380649a0e5592b7f054fd3f30
Rico Moorer <rico@alumni.upenn.edu>
2016-01-05 17:49:05 -0800

    Added MixPanel image

--- src/Footer.js
+++ src/Footer.js
@@ -4,11 +4,17 @@ export default class Footer extends Component {
   trackDonate() {
     mixpanel.track('Clicked Donate');
   }
-  
+
   render() {
     let styleObj = {
       textAlign: 'center',
     };
+
+    let mixPanelImg = {
+      margin: 'auto',
+      paddingRight: '30px'
+    };
+
     return (
       <div style={styleObj} id='footer-section'>
         <div className='footerFlexContainer'>
@@ -17,6 +23,9 @@ export default class Footer extends Component {
           </span>
           <span style={styleObj} className='footerFlexItem' id='brought'>Brought to you by the good folks at Evil Corp</span>
           <a onClick={this.trackDonate} className='footerFlexItem' target="_blank" href='https://shop.stjude.org/GiftCatalog/donation.do?cID=13805&pID=24591&sc_icid=lz-tg-btn-donate-now'>Donate Here</a>
+        </div>
+        <div className='footerFlexContainer'>
+        <a style={mixPanelImg} href="https://mixpanel.com/f/partner" rel="nofollow"><img src="//cdn.mxpnl.com/site_media/images/partner/badge_light.png" alt="Mobile Analytics" /></a>
         </div>
       </div>
     )
