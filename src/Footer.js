e0d3d82a9fcbf4f005cd5d54c6e6d3239a6f2d55
Susan Tashiro <susantashiro@gmail.com>
2016-01-07 18:01:23 -0800

    linkedin links

--- src/Footer.js
+++ src/Footer.js
@@ -11,7 +11,7 @@ export default class Footer extends Component {
           <span className='footerFlexItem'>
             <a id='feedback' href='' onClick={this.props.formModal}>Feedback</a>
           </span>
-          <span style={styleObj} className='footerFlexItem' id='brought'>Brought to you by the good folks at Evil Corp</span>
+          <span style={styleObj} className='footerFlexItem' id='brought'>Brought to you by <a href='http://www.linkedin.com/in/briangrober'>Brian</a>, <a href='http://www.linkedin.com/in/ricomoorer'>Rico</a> and <a href='http://www.linkedin.com/in/susantashiro'>Susan</a> </span>
           <a className='footerFlexItem' target="_blank" href='https://shop.stjude.org/GiftCatalog/donation.do?cID=13805&pID=24591&sc_icid=lz-tg-btn-donate-now'>Donate Here</a>
         </div>
       </div>
