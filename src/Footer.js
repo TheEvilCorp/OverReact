ded72ce574f430664b713c5151a365624720bbd7
Susan Tashiro <susantashiro@gmail.com>
2016-01-09 17:31:04 -0800

    Added Github link

--- src/Footer.js
+++ src/Footer.js
@@ -21,6 +21,7 @@ export default class Footer extends Component {
         <div className='footerFlexContainer'>
           <span className='footerFlexItem'>
             <a id='feedback' href='' onClick={this.props.formModal}>Feedback</a>
+            <a id='github' href='https://github.com/TheEvilCorp/OverReact'>Github</a>
           </span>
           <span style={styleObj} className='footerFlexItem' id='brought'>Brought to you by <a href='http://www.linkedin.com/in/briangrober' target="_blank">Brian</a>, <a href='http://www.linkedin.com/in/ricomoorer' target="_blank">Rico</a> and <a href='http://www.linkedin.com/in/susantashiro' target="_blank">Susan</a> </span>
           <a style={mixPanelImg} href="https://mixpanel.com/f/partner" rel="nofollow"><img src="//cdn.mxpnl.com/site_media/images/partner/badge_light.png" alt="Mobile Analytics" /></a>
